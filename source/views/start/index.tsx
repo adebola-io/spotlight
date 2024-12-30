c39b9ea09af948e01119c9098555cff273af22ec
adebola-io <adebolaakomolafe@gmail.com>
2024-12-30 15:11:14 +0100

    Remove overscroll

--- source/views/start/index.tsx
+++ source/views/start/index.tsx
@@ -2,7 +2,7 @@ import { For } from '@adbl/unfinished';
 import classes from './styles.module.scss';
 
 export default function Start() {
-  const count = Array(60).keys();
+  const count = Array(80).keys();
   return (
     <div class={classes.gridContainer}>
       <div class={classes.grid}>
